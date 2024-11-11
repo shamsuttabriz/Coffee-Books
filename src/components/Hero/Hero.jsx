1537b22ed0fd8284171e34e6a53add30af0c3b62
shamsuttabriz <shamsuttabriz.md@gmail.com>
2024-11-12 02:26:17 +0600

    router setup completed

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,7 @@
+export default function Hero() {
+  return (
+    <div>
+      <h1>Welcome to Hero</h1>
+    </div>
+  );
+}
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,7 @@
+export default function Navbar() {
+  return (
+    <div>
+      <h1>Welcome to Navbar</h1>
+    </div>
+  );
+}
